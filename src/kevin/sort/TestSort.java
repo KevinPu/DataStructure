98ed706f20c6db08a3ba3af3226196691903c7a9
Kevin Pu <pukaib@foxmail.com>
2018-03-27 01:06:41 +0800

    m

--- src/kevin/sort/TestSort.java
+++ src/kevin/sort/TestSort.java
@@ -1,21 +1,18 @@
 package kevin.sort;
 
-import org.junit.Test;
-
 public class TestSort {
-	
-	@Test
-	public void test() {
-		Integer[] a = {34, 8, 64, 51, 32, 21, 1, 13, 24, 26, 38, 15, 21, 82};
-		
+
+    public static void main(String[] args) {
+        Integer[] a = {34, 8, 64, 51, 32, 21, 1, 13, 24, 26, 38, 15, 21, 82};
+
 //		Sort.insertSort(a);
 //		Sort.bubbleSort(a);
 //		Sort.selectSort(a);
 //		Sort.shellSort(a);
-		Sort.mergeSort(a);
-		
-		for (int i = 0; i < a.length; i++) {
-			System.out.print(a[i] + " ");
-		}
-	}
+        Sort.mergeSort(a);
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
 }
